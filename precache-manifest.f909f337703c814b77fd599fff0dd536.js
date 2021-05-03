318a8252ad8a8635e2eefd18e08f0c9649853b20
Taronshah <shah.taron@gmail.com>
2021-05-03 12:24:54 +0400

    Updates

---
+++ precache-manifest.f909f337703c814b77fd599fff0dd536.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6c1022948fed07cae17c5f9d37064f4",
+    "url": "/e-commerce-web-shop/index.html"
+  },
+  {
+    "revision": "09f0e561077adb7c93d0",
+    "url": "/e-commerce-web-shop/static/js/2.4ba40216.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/e-commerce-web-shop/static/js/2.4ba40216.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e584a0522e2bff21792",
+    "url": "/e-commerce-web-shop/static/js/main.0d170a0d.chunk.js"
+  },
+  {
+    "revision": "ff8088f2de142a4385b8",
+    "url": "/e-commerce-web-shop/static/js/runtime-main.c4ed27c0.js"
+  },
+  {
+    "revision": "422fec1079cce450353e924288f8313b",
+    "url": "/e-commerce-web-shop/static/media/commerce.422fec10.png"
+  }
+]);
